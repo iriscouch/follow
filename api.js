5db262847d1c9efe61e2b5227250d121b24ddd61
Jason Smith <jhs@iriscouch.com>
2011-06-08 12:39:02 +0000

    Functional API like request

--- api.js
+++ api.js
@@ -3,4 +3,13 @@
 
 var feed = require('./feed');
 
-module.exports = feed;
+function follow_feed(opts, cb) {
+  var ch_feed = new feed.Feed(opts);
+  ch_feed.on('error' , function(er) { return cb && cb(er) });
+  ch_feed.on('change', function(ch) { return cb && cb(null, ch) });
+  ch_feed.follow();
+  return ch_feed;
+}
+
+module.exports = follow_feed;
+module.exports.Feed = feed.Feed;
