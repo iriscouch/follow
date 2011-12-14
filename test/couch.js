408f11f9ee58d19dd2076fd48ccdce613ed5e273
Jason Smith (air) <jhs@iriscouch.com>
2011-12-14 08:15:23 +0700

    A no-op test file

---
+++ test/couch.js
@@ -0,0 +1,11 @@
+var tap = require('tap')
+  , test = tap.test
+  , util = require('util')
+  , request = require('request')
+
+var api = require('../api')
+
+
+test('Follow API', function(t) {
+  t.end()
+})
