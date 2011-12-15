c30edae319c729e20a5e38b59fe6c32f94772fcb
Jason Smith (air) <jhs@iriscouch.com>
2011-12-15 10:19:01 +0700

    Set up the test database

--- test/couch.js
+++ test/couch.js
@@ -3,9 +3,46 @@ var tap = require('tap')
   , util = require('util')
   , request = require('request')
 
-var api = require('../api')
+var follow = require('../api')
+  , DB = process.env.db || 'http://localhost:5984/follow_test'
+  , RTT
 
+test('Couch is there', function(t) {
+  request.del({uri:DB, json:true}, function(er, res) {
+    if(er) throw er;
+    if(!res.body || res.body.error && res.body.error != 'not_found')
+      throw new Error('Failed to delete: ' + DB)
+
+    request.put({uri:DB, json:true}, function(er, res) {
+      if(er) throw er;
+      if(!res.body || res.body.error && res.body.error != 'not_found')
+        throw new Error('Failed to create: ' + DB)
+
+      var begin = new Date
+      var values = ['first', 'second', 'third']
+      values.forEach(function(val) {
+        var doc = { _id:'doc_'+val, value:val }
+        request.post({uri:DB, json:doc}, posted)
+      })
+
+      var count = 0;
+      function posted(er, res) {
+        if(er) throw er;
+        if(res.statusCode != 201)
+          throw new Error('POST document failure')
+
+        count += 1
+        if(count == values.length) {
+          RTT = (new Date) - begin
+          t.end()
+        }
+      }
+    })
+  })
+})
 
 test('Follow API', function(t) {
+  t.ok(RTT, 'The previous test set the base round trip time')
+
   t.end()
 })
